9d126aa4408aeec47a89170d99b575aabbd34fea
Gustav Behm <me@rootmos.io>
2026-02-01 14:33:27 +0100

    Use the buffer's readonly flag to decide when to update the twitch file

--- browse/nvim/ftplugin/twitch.lua
+++ browse/nvim/ftplugin/twitch.lua
@@ -18,9 +18,29 @@ vim.wo.spell = false
 vim.o.laststatus = 0
 vim.o.cmdheight = 0
 
-local period = 60*1000
-vim.uv.new_timer():start(period, period, function()
-    vim.schedule(function()
-        vim.api.nvim_command("checktime")
+if vim.bo.readonly then
+    local period = 60*1000
+    vim.uv.new_timer():start(period, period, function()
+        vim.schedule(function()
+            vim.api.nvim_command("checktime")
+        end)
     end)
-end)
+else
+    vim.api.nvim_create_autocmd({ "BufWritePost", "FileWritePost" }, {
+        buffer = 0,
+        callback = function()
+            local path = vim.api.nvim_buf_get_name(0)
+            vim.uv.spawn("/home/gustav/.local/bin/twitch", {
+                args = { "videos-file", "--in-place", path },
+                stdio = { nil, nil, 2 },
+                detached = true,
+            }, function(code, signal)
+                if code == 0 then
+                    vim.schedule(function()
+                        vim.api.nvim_command("checktime")
+                    end)
+                end
+            end)
+        end,
+    })
+end
